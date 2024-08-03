08ba8d557336e181ea692004df6586f1778d25b1
Zenish Sangani <jenishsangani54@gmail.com>
2024-08-03 10:52:04 +0530

    add mongodb url

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const listingRouter=require("./routes/listing.js")
 const reviewRouter=require("./routes/review.js")
 const userRouter=require("./routes/user.js")
 
-const dbUrl="mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl=process.env.ATLASDB_URL;
 
 async function main(){
    await mongoose.connect(dbUrl);
